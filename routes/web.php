f118b8414d8280728e0f123760a94dc381905119
yuji <y.you.sapo@gmail.com>
2022-01-27 23:07:15 +0900

    providertest

--- routes/web.php
+++ routes/web.php
@@ -26,5 +26,6 @@ Route::get('/dashboard', function () {
 Route::get('/component-test1', [ComponentTestController::class, 'showComponent1']); //跳び先のファイル名（component-test1）、読み込むコントローラー名（ComponentTestController）メソッド指定（showComponent1）
 Route::get('/component-test2', [ComponentTestController::class, 'showComponent2']);
 Route::get('/servicecontainertest', [LifeCycleTestController::class, 'showServiceContainerTest']);
+Route::get('/serviceprovidertest', [LifeCycleTestController::class, 'showServiceProviderTest']);
 
 require __DIR__.'/auth.php';
